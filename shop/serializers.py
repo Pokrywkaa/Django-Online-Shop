b7183b7efb59d0e86ef1155c7d24898ef61a87ad
Pokrywkaa <damian.pokrywka3@gmail.com>
2022-09-16 19:13:35 +0200

    add nested serializer

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,17 +1,19 @@
+from unicodedata import category
 from rest_framework import serializers
 from .models import Product, Category
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(max_length=None,
-                                        allow_null=True,
-                                        required=False)
-    class Meta:
-        model = Product
-        fields = '__all__'
-
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(max_length=None,
+                                        allow_null=True,
+                                        required=False)
+    category = CategorySerializer()
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
