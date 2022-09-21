078cced4c8f3fda7bc8beb463af0fa5da49a2980
Pokrywkaa <damian.pokrywka3@gmail.com>
2022-09-21 23:14:15 +0200

    add templates

--- shop/urls.py
+++ shop/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.ProductList.as_view(), name='product_list'),
-    path('<slug:category_slug>/', views.ProductList.as_view(),
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list,
         name='product_list_by_category'),
-    path('<int:id>/<slug:slug>/', views.ProductDetail.as_view(),
+    path('<int:id>/<slug:slug>/', views.product_detail,
         name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,26 +1,27 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product, Category
-from .serializers import ProductSerializer, CategorySerializer
 
-class ProductList(APIView):
-    def get(self, request, category_slug=None):
+
+def product_list(request, category_slug=None):
         category = None
         categories = Category.objects.all()
         products = Product.objects.filter(available=True)
         if category_slug:
             category = get_object_or_404(Category, slug=category_slug)
             products = products.filter(category=category)
-        serializer = ProductSerializer(products, many=True, context={"request":request})
-        return Response(serializer.data)
+        return render(request,
+                    'shop/product/list.html',
+                    {'category': category,
+                        'categories': categories,
+                        'products': products})
 
-class ProductDetail(APIView):
-    def get(self, request, id, slug):
-        product = get_object_or_404(Product,
-                                    id=id,
-                                    slug=slug,
-                                    available=True)
-        serializer = ProductSerializer(product, context={"request":request})
-        return Response(serializer.data)
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})
