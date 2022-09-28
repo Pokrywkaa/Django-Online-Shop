3aa0fab1fe72f5adda385e215ced379f12a72538
Damian Pokrywka <damian.pokrywka@capgemini.com>
2022-09-28 22:17:30 +0200

    pdf

--- orders/admin.py
+++ orders/admin.py
@@ -33,6 +33,11 @@ def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
@@ -41,7 +46,7 @@ class OrderItemInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated', order_detail]
+                    'created', 'updated', order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
---
+++ payment/tasks.py
@@ -0,0 +1,32 @@
+import weasyprint
+
+from io import BytesIO
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+
+from myshop.celery import app
+from orders.models import Order
+
+@app.task
+def payment_completed(order_id):
+    order = Order.objects.get(id=order_id)
+    # create invoice e-mail
+    subject = f'My Shop - EE Invoice no. {order.id}'
+    message = 'Please, find attached the invoice for your recent 4iu purchase.'
+    email = EmailMessage(subject,
+                        message,
+                        'admin@myshop.com',
+                        [order.email])
+    # generate PDF
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    out = BytesIO()
+    stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+    weasyprint.HTML(string=html).write_pdf(out,
+                                        stylesheets=stylesheets)
+    # attach PDF file
+    email.attach(f'order_{order.id}.pdf',
+                out.getvalue(),
+                'application/pdf')
+    # send e-mail
+    email.send()
